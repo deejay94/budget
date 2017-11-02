66f60bf9e340df8f8781e738f4618a03bc7d6f3d
deejay94 <dahjoyner12@gmail.com>
2017-11-02 15:23:54 -0400

    Ember deploy

--- config/environment.js
+++ config/environment.js
@@ -1,8 +1,7 @@
 /* jshint node: true */
 
-module.exports = function (environment) {
-  'use strict';
-  const ENV = {
+module.exports = function(environment) {
+  var ENV = {
     modulePrefix: 'budget',
     environment: environment,
     rootURL: '/',
@@ -11,7 +10,7 @@ module.exports = function (environment) {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
-      },
+    },
       EXTEND_PROTOTYPES: {
         // Prevent Ember Data from overriding Date.parse.
         Date: false,
@@ -49,8 +48,10 @@ module.exports = function (environment) {
   }
 
   if (environment === 'production') {
-    ENV.locationType = 'hash';
-  }
+  ENV.rootURL = '/budget';
+  ENV.locationType = 'hash';
+  ENV.apiHost = 'https://budget1-api.herokuapp.com/';
+}
 
   return ENV;
 };
