fe10f619cef3c973b244afa4bea6c3fd1c0c1b6d
deejay94 <dahjoyner12@gmail.com>
2017-11-02 09:50:18 -0400

    Add 'delete' button to 'budget' route
    -Create 'delete' button in 'budget-list' hbs
    -Add action deleteBudget
    -send up action and budget to 'budget' route
    -Add delete function in 'budget' js
    
    Next step is to update a list name

--- app/components/budget-list.js
+++ app/components/budget-list.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    deleteBudget () {
+      return this.sendAction('deleteBudget', this.get('budget'));
+    }
+  }
 });
