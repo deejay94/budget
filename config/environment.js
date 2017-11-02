453e91b15dc5f4338629fcee40979ace2e5a0f53
deejay94 <dahjoyner12@gmail.com>
2017-11-02 16:31:34 -0400

    Remove erroneous extra closing bracket
    - Must have been pulled in from elsewhere, breaks build

--- config/environment.js
+++ config/environment.js
@@ -15,7 +15,6 @@ module.exports = function(environment) {
       EXTEND_PROTOTYPES: {
         // Prevent Ember Data from overriding Date.parse.
         Date: false,
-      },
     },
 
     APP: {
