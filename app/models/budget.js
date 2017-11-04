f03fd854e93bb4fcdcb9f973fc1b6dc37b297067
deejay94 <dahjoyner12@gmail.com>
2017-11-04 17:25:55 -0400

    Add computed property and change color of msg
    -Add computed property 'isOverBudget'
    -Refactor hbs template with if else statement with 'p' tags
    -Add class to 'p' tags
    -Create scss file
    -Add class to file and make colors green and red

--- app/models/budget.js
+++ app/models/budget.js
@@ -5,16 +5,14 @@ export default DS.Model.extend({
   goal: DS.attr('number'),
   actual: DS.attr('number'),
   user: DS.belongsTo('user'),
-
   difference: Ember.computed('goal', 'actual', function() {
     let goal = this.get('goal');
     let actual = this.get('actual');
-
-      if (goal > actual) {
-        return `You spent $${(goal - actual)} less than your goal! Good job!`
-        // return ('You spent $' + (goal-actual) + ' less than your goal! Good job!')
-      } else {
-        return `You spent $${(actual - goal)} more than your goal! Do better!`
-      }
-    })
+    return Math.abs(goal - actual);
+  }),
+  isOverBudget: Ember.computed('goal', 'actual', function() {
+    let goal = this.get('goal');
+    let actual = this.get('actual');
+    return goal < actual;
+  })
 })
