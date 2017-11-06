0c24dadc93c1e552a635a74d6a6911b60e38d1f6
deejay94 <dahjoyner12@gmail.com>
2017-11-06 16:30:53 -0500

    Add div to hbs for 'stats' route
    -Add border around 'green' and 'red' class

--- app/models/user.js
+++ app/models/user.js
@@ -2,7 +2,8 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   email: DS.attr('string'),
-  budgets: DS.hasMany('budget')
+  budgets: DS.hasMany('budget'),
+  totalBudgetGoals: Ember.computed('budgets.@each.goal', function() {
+     return this.get('budgets').mapBy('goal').reduce((a, b) => a + b, 0);
+ })
 });
-
-totalGoal: Ember.computed.sum('goal');
