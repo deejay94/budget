12fe2a60de1eb20d13414dd0b92f6857808aa5f1
deejay94 <dahjoyner12@gmail.com>
2017-11-06 10:56:58 -0500

    Add error handling to create form
    -Add promises to handle errors

--- app/routes/new.js
+++ app/routes/new.js
@@ -7,13 +7,11 @@ export default Ember.Route.extend({
       newBudget.save()
       .then(() => {
         this.get('flashMessages').success('Successfully added new Budget');
+        this.transitionTo('budgets');
       })
       .catch(() => {
         newBudget.rollbackAttributes();
         this.get('flashMessages').danger('Failed to add new Budget, please check all required fields.');
-      })
-      .then(() => {
-        this.transitionTo('budgets');
       })
         },
       cancel(budget) {
